3c459d298710bda4f1daec7b69995d686178a8d3
Philip (Yinuo) Shih <philipshih1@gmail.com>
2020-04-12 11:48:09 -0700

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
   particlesJS('particles-js', {
     "particles": {
       "number": {
-        "value": 30,
+        "value": 35,
         "density": {
           "enable": true,
           "value_area": 800
@@ -77,7 +77,7 @@ document.addEventListener("DOMContentLoaded", function () {
       "detect_on": "canvas",
       "events": {
         "onhover": {
-          "enable": false,
+          "enable": true,
           "mode": "grab"
         },
         "onclick": {
